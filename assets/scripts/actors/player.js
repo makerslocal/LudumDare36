8ae5603baaba20cc0c34cde9cd21859ba3edb764
Jake Harris <milieu@javakat.io>
2016-08-27 18:29:54 -0500

    Stub Player class

---
+++ assets/scripts/actors/player.js
@@ -0,0 +1,9 @@
+class Player {
+    constructor () {
+        this.name = 'Dumb Name'
+        this.city = null
+        this.upgrades = {}
+        this.health = 100
+        this.money = 100
+    }
+}
