f1e6137388dd3fd5ccd803f00cd2b65819d02cbe
Jake Harris <milieu@javakat.io>
2016-08-28 14:25:55 -0500

    Add cute Sign-kun, fix input

--- assets/scripts/stages/start.js
+++ assets/scripts/stages/start.js
@@ -5,10 +5,10 @@ class Start {
     
     preload () {
         this.game.load.image('start', 'assets/sprites/start.png')
+        this.game.load.spritesheet('sign', 'assets/sprites/start-button.png', 48, 58)
     }
     create () {
         this.game.add.image(0, 0, 'start')
-        this.game.input.enabled = true
         this.game.input.keyboard.onUpCallback = function (e) {
             this.beginGame()
         }.bind(this)
@@ -16,10 +16,16 @@ class Start {
             this.beginGame()
         }.bind(this)
         
-        this.overlay = this.game.add.graphics(0, 0)
-        this.overlay.opacity = 0
         this.timer = this.game.time.create(false)
         this.timer.loop(Start.FADE_TIMEOUT, this.beginGame.bind(this))
+        
+        this.sign = this.game.add.sprite(this.game.camera.width * 1/2 + 50, this.game.camera.height * 4/6, 'sign', 0)
+        this.sign.scale.setTo(2, 2)
+        this.sign.animations.add('idle')
+        this.sign.animations.play('idle', 5, true)
+        
+        this.overlay = this.game.add.graphics(0, 0)
+        this.overlay.opacity = 0
     }
     update () {
         
@@ -33,8 +39,11 @@ class Start {
     }
     
     beginGame () {
-        if(this.overlay.opacity >= 1)
+        if(this.overlay.opacity >= 1) {
+            this.game.input.keyboard.onUpCallback = function () {}
+            this.game.input.mouse.mouseUpCallback = function () {}
             this.game.state.start('overworld')
+        }
         else this.overlay.opacity += 0.1
         
         this.timer.start()
