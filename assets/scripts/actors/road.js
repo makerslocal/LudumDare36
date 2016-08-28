40eaebdec01dd14c6e2632f344aaaa4f6b4dbda3
Jake Harris <milieu@javakat.io>
2016-08-28 04:03:29 -0500

    Make roads great again

--- assets/scripts/actors/road.js
+++ assets/scripts/actors/road.js
@@ -5,9 +5,16 @@ class Road {
         this.cf = cf
         
         this.line = new Phaser.Line(co.x, co.y, cf.x, cf.y)
+        this.length = this.getTravelDuration()
     }
     
     isConnectedTo(city) {
         return this.co === city || this.cf === city
+    }
+    getLength() {
+		return Math.hypot(this.co.x - this.cf.x, this.co.y - this.cf.y); //lol
+	}
+    getTravelDuration() {
+        return Math.ceil(this.getLength() / (100 + Math.floor(100 * Math.random())))
     }
 }
