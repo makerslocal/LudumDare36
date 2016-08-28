2f10c6bb6ad79d02dfe95479bd0aa9cbc1806f9c
Jake Harris <milieu@javakat.io>
2016-08-27 19:21:15 -0500

    Make City extend Phaser.Sprite for ease of use/consistency

--- assets/scripts/actors/city.js
+++ assets/scripts/actors/city.js
@@ -1,21 +1,16 @@
-class City {
+class City extends Phaser.Sprite {
 	constructor(name,x,y,game) {
-		this.name = name;
-		this.x = x;
-		this.y = y;
         
-        this.sprite = game.add.sprite(
-            Math.floor(this.x * game.world.width), 
-            Math.floor(this.y * game.world.height),
-            'city'
-        );
-        this.sprite.name = this.name
-        this.sprite.inputEnabled = true;
-        this.sprite.input.useHandCursor = true;
-        this.sprite.events.onInputUp.add(function (e) {
-            e.game.state.clickedCity = e;
-            e.game.state.current = e
-            console.log(e);
+        super(game, Math.floor(x * game.world.width), Math.floor(y * game.world.height), 'city')
+        this.game.add.existing(this)
+        
+		this.name = name;
+        
+        this.inputEnabled = true;
+        this.input.useHandCursor = true;
+        this.events.onInputUp.add(function (e) {
+            this.game.state.states.overworld.clickedCity = this;
+            this.game.state.states.overworld.current = this;
         }, this);
 	}
 }
