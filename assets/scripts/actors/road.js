3493d67d7758b9a94bd616b31916e1c5faaae5a1
Jake Harris <milieu@javakat.io>
2016-08-28 16:57:40 -0500

    Generate treachery for roads

--- assets/scripts/actors/road.js
+++ assets/scripts/actors/road.js
@@ -6,6 +6,34 @@ class Road {
         
         this.line = new Phaser.Line(co.x, co.y, cf.x, cf.y)
         this.length = this.getTravelDuration()
+        
+        this.treachery = this.generateTreachery()
+    }
+    
+    generateTreachery() {
+        var r = Math.random()
+        
+        r -= Road.GENTLE_CHANCE
+        if(r <= 0) 
+            return 1
+            
+        r -= Road.STANDARD_CHANCE
+        if(r <= 0)
+            return 2
+            
+        r -= Road.DIFFICULT_CHANCE
+        if(r <= 0)
+            return 3
+            
+        r -= Road.GRUELING_CHANCE
+        if(r <= 0)
+            return 4
+            
+        r -= Road.LETHAL_CHANCE
+        if(r <= 0)
+            return 5
+            
+        return 1
     }
     
     isConnectedTo(city) {
@@ -18,3 +46,9 @@ class Road {
         return Math.ceil(this.getLength() / (100 + Math.floor(100 * Math.random())))
     }
 }
+    
+Road.GENTLE_CHANCE = .3
+Road.STANDARD_CHANCE = .2
+Road.DIFFICULT_CHANCE = .2
+Road.GRUELING_CHANCE = .2
+Road.LETHAL_CHANCE = .1
