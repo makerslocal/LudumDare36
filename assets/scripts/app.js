91fbcac839cac6c7cc5203f3f60fc374e14fdec2
Jake Harris <milieu@javakat.io>
2016-08-29 17:44:45 -0500

    Implement game overs and resets

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,6 +13,7 @@ WebFont.load(wfconfig);
 
 var game = new Phaser.Game(1024, 640)
 
+game.state.add('game-over', GameOver, true)
 game.state.add('upgrade', Upgrade, true)
 game.state.add('travel', Travel, true)
 game.state.add('overworld', Overworld, true)
