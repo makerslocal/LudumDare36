608610e20c5bab497863b355fdb4b1c242ba5520
Tyler Crumpton <tyler.crumpton@gmail.com>
2016-08-29 02:28:04 -0500

    Fix fonts not loading at the beginning.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var wfconfig = {
+    active: function() {
+        console.log("font loaded");
+    },
+    google: {
+        families: ['Amatic SC', 'Inconsolata']
+    }
+};
+
+WebFont.load(wfconfig);
+
 var game = new Phaser.Game(1024, 640)
 
 // game.state.add('upgrade', Upgrade, true)
