b071a8b0090a8c623fdd5e94b89dcf3012f3833e
Tyler Crumpton <tyler.crumpton@gmail.com>
2016-08-29 02:13:05 -0500

    Properly center text above cities.

--- assets/scripts/actors/city.js
+++ assets/scripts/actors/city.js
@@ -18,9 +18,16 @@ class City extends Phaser.Sprite {
             if(this instanceof City) this.game.state.states.overworld.clickedCity = this;
             this.game.state.states.overworld.current = this;
         }, this);
-        
-        this.nameText = this.game.add.text(this.x + (this.width / 2) - (11 * this.name.length) / 2, this.y + 35, this.name, { font: 'Amatic SC, sans-serif', fill: '#211c05', stroke: '#FFB707', fontSize: '24px', fontWeight: 'bold'})
-        
+
+        this.nameText = this.game.add.text(0, 0,
+                                           this.name,
+                                           { font: 'Amatic SC, sans-serif',
+                                             fill: '#211c05', stroke: '#FFB707',
+                                             fontSize: '24px', fontWeight: 'bold',
+                                             boundsAlignH: 'center',
+                                             boundsAlignV: 'middle' })
+        this.nameText.setTextBounds(this.x + this.width/2 - 100, this.y-30, 200, 50);
+        this.nameText.setShadow(1, 1, 'rgba(0,0,0,0.5)', 2);
         this.alpha = 0.4
         this.nameText.alpha = 0
     }
